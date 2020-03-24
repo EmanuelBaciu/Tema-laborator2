f2c07433c3675e9019ae9231a0fe2abde45c0520
EmanuelBaciu <62619090+EmanuelBaciu@users.noreply.github.com>
2020-03-24 21:16:29 +0200

    Add files via upload

---
+++ PersoaneContact.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieTipAgenda
+{
+    /// <summary>
+    /// Persoanele de contact din agenda
+    /// </summary>
+   public class PersoaneContact
+    {
+        public string nume;
+        public string numarTelefon;
+        public string adresaEmail;
+
+        public DateTime DataNasterii;
+        /// <summary>
+        /// Constructor cu parametri
+        /// </summary>
+        /// <param name="numePersoana"></param>
+        /// <param name="nrTel"></param>
+        /// <param name="e_mail"></param>
+        public PersoaneContact(string numePersoana, string nrTel, string e_mail)
+        {
+            nume = numePersoana;
+            numarTelefon = nrTel;
+            adresaEmail = e_mail;
+
+        }
+        public string Info()
+        {
+            return "Persoana de contact are numele " + nume + " cu numarul de telefon: " + numarTelefon + " si adresa de e-mail: " + adresaEmail;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieTipAgenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersoaneContact persoana = new PersoaneContact("Emanuel", "0743307248", "emy_baciu@yahoo.com");
+
+            
+            Console.WriteLine(persoana.Info());
+
+            Console.ReadKey();
+
+        }
+    }
+}
